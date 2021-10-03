068b36224cbf55291cf245f52fb57f8dc05ffa41
Earth <5e-earth@protonmail.com>
2021-10-03 16:48:52 +0200

    fix linting

--- src/components/helper/LogoComponent.tsx
+++ src/components/helper/LogoComponent.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const LogoComponent: React.FunctionComponent = () => (
   <a href="/">
     <img
-      src="https://dzqc5mj6l3nh2.cloudfront.net/images/5e-logo-only-gold.png" 
-      alt="Five Elements Token Logo"      
-      className="object-contain h-16 sm:h-24 " 
+      src="https://dzqc5mj6l3nh2.cloudfront.net/images/5e-logo-only-gold.png"
+      alt="Five Elements Token Logo"
+      className="object-contain h-16 sm:h-24"
     />
   </a>
 );
