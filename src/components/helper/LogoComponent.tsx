d494aecf28b23d402288e44d6149a9e2debcf3e5
Earth <5e-earth@protonmail.com>
2021-10-03 16:44:47 +0200

    responsive logo for mobile devices

--- src/components/helper/LogoComponent.tsx
+++ src/components/helper/LogoComponent.tsx
@@ -4,9 +4,8 @@ const LogoComponent: React.FunctionComponent = () => (
   <a href="/">
     <img
       src="https://dzqc5mj6l3nh2.cloudfront.net/images/5e-logo-only-gold.png" 
-      alt="Five Elements Token Logo"
-      width="120px"
-      height="120px" 
+      alt="Five Elements Token Logo"      
+      className="object-contain h-16 sm:h-24 " 
     />
   </a>
 );
