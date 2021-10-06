1428be5827be5aeb3c97d9516cff74b66900d31d
Earth <5e-earth@protonmail.com>
2021-10-06 20:56:51 +0200

    minor styling changes for logo and cards

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -3,8 +3,9 @@ import './card.scss';
 
 const CardComponent: React.FunctionComponent = ({ children }) => (
   <div
-    className={`transition duration-500 ease-in-out md:opacity-50
-      transform  md:hover:scale-125 md:hover:opacity-100
+    className={`border-2 border-transparent sm:rounded-lg sm:hover:border-token md:hover:shadow-lg
+      transition duration-500 ease-in-out md:opacity-50
+      transform md:hover:scale-125 md:hover:-translate-y-2 md:hover:opacity-100 md:skew-y-3 md:hover:skew-y-0
       card cursor-pointer p-6 md:p-3 md:m-3 text-center bg-card-background
     `}
   >
